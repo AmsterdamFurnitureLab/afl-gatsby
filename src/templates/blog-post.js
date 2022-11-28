68e36dd849ccc33c4a5aa8b3ca980c68c17ff917
Julia van der Kris <julia@juuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuulia.dev>
2022-11-28 16:43:41 +0100

    Update to Gatsby 5.2.0

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Img from "gatsby-image"
+import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -31,7 +31,7 @@ class BlogPostTemplate extends React.Component {
 
           {post.frontmatter.thumbnail && (
             <div className="post-content-image">
-              <Img
+              <StaticImage
                 className="kg-image"
                 fluid={post.frontmatter.thumbnail.childImageSharp.fluid}
                 alt={post.frontmatter.title}
